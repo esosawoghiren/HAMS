bf333cdb186766f75f8e6c12dff04fc1409dc28a
esosawoghiren <esosa.w@hotmail.com>
2023-10-03 12:59:21 -0400

    updated homemenu signup button

--- app/src/main/java/com/example/hams/HomeMenu.java
+++ app/src/main/java/com/example/hams/HomeMenu.java
@@ -2,13 +2,30 @@ package com.example.hams;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeMenu extends AppCompatActivity {
+    private Button button_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_menu2);
+        button_signup=findViewById(R.id.Signup);
+        button_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegisterpage();
+            }
+        });
+
+    }
+
+    public void openRegisterpage(){
+        Intent intent= new Intent (this, RegisterMenuFragment.class);
+        startActivity(intent);
     }
 }
