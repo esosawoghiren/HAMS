4770bc82025e69a7a44b11780cbb880929994fd2
esosawoghiren <esosa.w@hotmail.com>
2023-10-03 11:45:15 -0400

    Initial commit

---
+++ app/src/main/java/com/example/hams/HomeMenu.java
@@ -0,0 +1,14 @@
+package com.example.hams;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_menu2);
+    }
+}
